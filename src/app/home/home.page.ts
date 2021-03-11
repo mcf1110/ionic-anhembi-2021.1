78c458adbcc47bc821e7c2fdd7dcb50a6ea2b2ea
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-03-10 21:08:42 -0300

    contador basico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public currentValue = 0;
+  public maxValue = 0;
+
+  public increment() {
+    this.currentValue++;
+    this.maxValue = Math.max(this.maxValue, this.currentValue);
+    // if (this.currentValue > this.maxValue) {
+    //   this.maxValue = this.currentValue;
+    // }
+  }
+
+  public decrement() {
+    this.currentValue = Math.max(0, this.currentValue - 1);
+    // if (this.currentValue > 0) {
+    //   this.currentValue--;
+    // }
+  }
+
+  public reset() {
+    this.currentValue = 0;
+  }
 
 }
