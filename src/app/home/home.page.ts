3f3b75bf17ecefd4abae84e44cf534a45b2d608f
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-03-16 20:04:26 -0300

    conversao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private unitToBit = {
+    'bit': 1,
+    'byte': 8,
+    'kilobit': 1024 ** 2,
+    'kilobyte': 8 * 1024 ** 2,
+    'megabit': 1024 ** 3,
+    'megabyte': 8 * 1024 ** 3,
+    'gigabit': 1024 ** 4,
+    'gigabyte': 8 * 1024 ** 4,
+    'terabit': 1024 ** 5,
+    'terabyte': 8 * 1024 ** 5,
+  }
 
-  constructor() {}
+  public input = 0;
+  public inputType = "";
+
+  public output = 0;
+  public outputType = "";
+
+  private calculateOutput() {
+    return this.input * this.unitToBit[this.inputType] / this.unitToBit[this.outputType];
+  }
+
+  public updateOutput() {
+    this.output = this.calculateOutput() || 0;
+  }
+
+  public swap() {
+    this.input = this.calculateOutput();
+    const tempType = this.outputType;
+    this.outputType = this.inputType;
+    this.inputType = tempType;
+  }
+
+  constructor() { }
 
 }
