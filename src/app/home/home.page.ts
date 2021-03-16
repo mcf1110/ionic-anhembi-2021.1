c7e02bcb7a2b9919bc243bd4eefe9b5bdeea8822
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-03-16 20:47:33 -0300

    delivery

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+type DeliveryStatus = 'finished' | 'preparing' | 'onRoute' | 'delivered';
+
+interface Delivery {
+  name: string;
+  status: DeliveryStatus;
+  sedex: boolean;
+  daysSinceBought: number;
+  estimatedDaysToDeliver: number;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +17,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public deliveries: Delivery[] = [
+    {
+      name: "TV",
+      status: "finished",
+      sedex: true,
+      daysSinceBought: 15,
+      estimatedDaysToDeliver: 14
+    }, {
+      name: 'Smartphone',
+      status: 'preparing',
+      sedex: false,
+      daysSinceBought: 20,
+      estimatedDaysToDeliver: 22
+    }
+  ]
+
+  constructor() { }
 
 }
