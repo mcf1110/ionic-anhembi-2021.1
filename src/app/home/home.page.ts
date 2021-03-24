ec7e2d66a02986c71cc4a4596cd8a2e1c3f5a271
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-03-24 20:09:39 -0300

    tarefa de casa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,8 +12,13 @@ interface Task {
 })
 export class HomePage {
 
-  public tasks: Task[] = []
+  public tasks: Task[] = [];
   public newTaskName = '';
+  public completedTaskPercentage = 0;
+  public currentFilter: 'all' | 'done' | 'todo' = 'all';
+  public currentSearch = '';
+
+  public filteredTasks = this.tasks;
 
   constructor() { }
 
@@ -21,8 +26,48 @@ export class HomePage {
     this.tasks.push({
       name: this.newTaskName,
       done: false
-    })
+    });
     this.newTaskName = '';
+
+    this.computeCompletedTaskPercentage();
+    this.updateFilter();
+  }
+
+  public computeCompletedTaskPercentage() {
+    // let completedTaskCount = 0;
+    // for (const task of this.tasks) {
+    //   if (task.done) {
+    //     completedTaskCount++;
+    //   }
+    // }
+    // this.completedTaskPercentage = completedTaskCount / this.tasks.length;
+    const completedTasks = this.tasks.filter(task => task.done);
+    this.completedTaskPercentage = completedTasks.length / this.tasks.length;
+  }
+
+  public removeTask(task: Task) {
+    const index = this.tasks.indexOf(task);
+    this.tasks.splice(index, 1);
+    this.updateFilter();
+  }
+
+  public updateFilter() {
+    let firstFilter: Task[];
+    if (this.currentFilter === 'all') {
+      firstFilter = this.tasks;
+    } else if (this.currentFilter === 'done') {
+      firstFilter = this.tasks.filter(task => task.done);
+    } else if (this.currentFilter === 'todo') {
+      firstFilter = this.tasks.filter(task => !task.done);
+    }
+
+    if (this.currentSearch === '') {
+      this.filteredTasks = firstFilter;
+    } else {
+      const lowercased = this.currentSearch.toLowerCase();
+      this.filteredTasks = firstFilter
+        .filter(task => task.name.toLowerCase().includes(lowercased));
+    }
   }
 
 }
