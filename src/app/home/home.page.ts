1847cf11bfe13fb40d6c2b9f1971d2fbafa7a91c
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-03-23 21:33:38 -0300

    funcao de limpar conta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 interface Transaction {
   date: Date;
@@ -16,12 +17,38 @@ export class HomePage {
   public selectedValue = 0;
   public transactions: Transaction[] = [];
 
+  constructor(private alertController: AlertController) { }
+
   public deposit() {
     this.counter += this.selectedValue;
     this.transactions.unshift({
       amount: this.selectedValue,
       date: new Date()
-    })
+    });
+    this.selectedValue = 0;
+  }
+
+  public async confirmWipe() {
+    const alert = await this.alertController.create({
+      header: 'Tem certeza?',
+      message: 'Essa operação não poderá ser desfeita!',
+      buttons: [
+        {
+          text: 'Sim',
+          handler: () => this.wipeAccount()
+        },
+        'Me enganei'
+      ]
+    });
+    alert.present();
+  }
+
+  private wipeAccount() {
+    this.transactions.unshift({
+      amount: -this.counter,
+      date: new Date()
+    });
+    this.counter = 0;
     this.selectedValue = 0;
   }
 
