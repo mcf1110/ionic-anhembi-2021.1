ecd53d52dc50a91a574cfd27107554e5006cdcbf
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-03-25 09:33:25 -0300

    parte 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public currentValue = 0;
+  public maxValue = 0;
+
+  constructor() { }
+
+  public increment() {
+    this.currentValue++;
+    // this.maxValue = Math.max(this.maxValue, this.currentValue);
+    if (this.currentValue > this.maxValue) {
+      this.maxValue = this.currentValue;
+    }
+  }
+
+  public reset() {
+    this.currentValue = 0;
+  }
+
+  public decrement() {
+    // this.currentValue = Math.max(this.currentValue - 1, 0);
+    if (this.currentValue > 0) {
+      this.currentValue--;
+    }
+  }
 
 }
