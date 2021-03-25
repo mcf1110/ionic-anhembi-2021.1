6b7cc09c24a160abccaf3ef6cf9130d4035c57ab
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-03-25 10:31:18 -0300

    conversao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+type Unit = 'bit' | 'byte' | 'kilobit' | 'kilobyte' | 'megabit' | 'megabyte' | 'gigabit' | 'gigabyte' | 'terabit' | 'terabyte';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public inputValue: number;
+  public inputType: Unit;
+  public outputValue: number;
+  public outputType: Unit;
+
+  private unitToBit = {
+    bit: 1,
+    byte: 8,
+    kilobit: 1024,
+    kilobyte: 1024 * 8,
+    megabit: 1024 ** 2,
+    megabyte: 1024 ** 2 * 8,
+    gigabit: 1024 ** 3,
+    gigabyte: 1024 ** 3 * 8,
+    terabit: 1024 ** 4,
+    terabyte: 1024 ** 4 * 8,
+  };
+
+  public calculateOutput() {
+    const bits = this.inputValue * this.unitToBit[this.inputType];
+    this.outputValue = bits / this.unitToBit[this.outputType];
+  }
 
 }
