8593daecd83a9fdd79b5f727123aeb321bc27ba8
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-04-07 19:31:13 -0300

    anhembi bank (:

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Transaction {
+  value: number;
+  date: Date;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -11,25 +16,21 @@ export class HomePage {
   public maxValue = 0;
   public selectedValue = 0;
 
+  public transactions: Transaction[] = []
+
   public increment() {
     this.currentValue += this.selectedValue;
     this.maxValue = Math.max(this.maxValue, this.currentValue);
+
+    this.transactions.push({
+      value: this.selectedValue,
+      date: new Date()
+    })
+
     this.selectedValue = 0;
     // if (this.currentValue > this.maxValue) {
     //   this.maxValue = this.currentValue;
     // }
-  }
-
-  public decrement() {
-    this.currentValue = Math.max(0, this.currentValue - this.selectedValue);
-    this.selectedValue = 0;
-    // if (this.currentValue > 0) {
-    //   this.currentValue--;
-    // }
-  }
-
-  public reset() {
-    this.currentValue = 0;
   }
 
 }
