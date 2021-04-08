5d1d9ee56a40ae32715a534ebefb41ba186abfb1
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-04-08 09:31:12 -0300

    tasks pt 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Task {
+  name: string;
+  done: boolean;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +12,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public tasks: Task[] = [];
+  public newTaskName = '';
+
+  constructor() { }
+
+  public addNewTask() {
+    if (this.newTaskName.trim().length == 0) {
+      return;
+    }
+    this.tasks.push({
+      name: this.newTaskName.trim(),
+      done: false
+    })
+    this.newTaskName = '';
+  }
 
 }
