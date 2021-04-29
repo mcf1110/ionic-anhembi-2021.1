93976380ceaef1217712e0364e08cecc54dd48c7
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-04-29 11:39:57 -0300

    offline

--- src/app/services/song.service.ts
+++ src/app/services/song.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular'
 
 export interface Song {
   id: number;
@@ -12,18 +13,18 @@ export interface Song {
 })
 export class SongService {
 
-  public songs: Song[] = [
-    { id: 1, artist: 'Rick Astley', title: 'Never Gonna Give You Up', isFavorite: true },
-    { id: 2, artist: 'Post Malone', title: 'Sunflower', isFavorite: false },
-    { id: 3, artist: 'StayC', title: 'ASAP', isFavorite: true },
-    { id: 4, artist: 'Charles Bradley', title: 'The World (Is Going Up In Flames)', isFavorite: false },
-    { id: 5, artist: 'Roddy Ricch', title: 'Every Season', isFavorite: false },
-    { id: 6, artist: 'Gamma Ray', title: 'Rebellion in Dreamland', isFavorite: false },
-    { id: 7, artist: 'Metallica', title: 'One', isFavorite: true },
-    { id: 8, artist: 'Britney Spears ', title: 'Oops! ... Did It Again', isFavorite: false },
-    { id: 9, artist: 'Ke$ha', title: 'Timber', isFavorite: false },
-    { id: 10, artist: 'Sabotage', title: 'Um bom lugar', isFavorite: true },
-  ];
+  public songs: Song[] = [];
+
+  private saveToStorage() {
+    this.storage.set('songs', this.songs);
+  }
+
+  private async loadFromStorage() {
+    const loadedSongs = await this.storage.get('songs');
+    if (loadedSongs) {
+      this.songs.push(...loadedSongs);
+    }
+  }
 
   public getSongById(id: number) {
     return { ...this.songs.find(s => s.id === id) };
@@ -32,12 +33,27 @@ export class SongService {
   public updateSong(updatedSong: Song) {
     const index = this.songs.findIndex(s => s.id === updatedSong.id);
     this.songs[index] = updatedSong;
+    this.saveToStorage();
   }
 
   public addSong(newSong: Song) {
     newSong.id = 1 + Math.max(0, ...this.songs.map(s => s.id));
     this.songs.push(newSong);
+    this.saveToStorage();
   }
 
-  constructor() { }
+  public deleteSong(id: number) {
+    const index = this.songs.findIndex(s => s.id === id);
+    this.songs.splice(index, 1);
+    this.saveToStorage();
+  }
+
+  public clearSongs() {
+    this.songs.splice(0, this.songs.length);
+    this.saveToStorage();
+  }
+
+  constructor(private storage: Storage) {
+    this.loadFromStorage();
+  }
 }
