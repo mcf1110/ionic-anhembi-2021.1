64e76a0a62fd417eee7d00c228f3a33d7763bba4
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-05-05 20:32:26 -0300

    listando stargazers

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+export interface GitHubUser {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
 
 @Component({
   selector: 'app-home',
@@ -7,6 +29,16 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.loadUsers();
+  }
+
+  public users: GitHubUser[];
+
+  public async loadUsers() {
+    const url = 'https://api.github.com/repos/mcf1110/ionic-anhembi-2021.1/stargazers';
+    const result = await this.http.get<GitHubUser[]>(url).toPromise(); // converte em promise
+    this.users = result;
+  }
 
 }
