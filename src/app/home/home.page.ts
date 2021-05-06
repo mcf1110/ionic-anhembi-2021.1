b0890e0b460379a5bab1a53745b70fa13852f48c
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-05-06 11:09:46 -0300

    conversor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+export interface BitCoin {
+  bpi: Record<Code, Rate>;
+}
+
+export interface Rate {
+  code: Code;
+  symbol: string;
+  rate: string;
+  description: string;
+  rate_float: number;
+}
+
+type Code = 'USD' | 'GBP' | 'EUR';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +22,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public value = 0;
+  public rates: Rate[];
+
+  constructor(private http: HttpClient) {
+    this.loadRate();
+  }
+
+  private async loadRate() {
+    const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+    const r = await this.http.get<BitCoin>(url).toPromise();
+    this.rates = Object.values(r.bpi);
+  }
 
 }
