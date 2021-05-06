36e918b19cff8a66001c49c65175f58bc879f89b
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-05-05 21:03:37 -0300

    detalhes do usuário

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'details',
+    loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
+  },
 ];
 
 @NgModule({
