f129ab8ee73c48fe5fe067e27d15a6e2d6c37094
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-05-05 21:20:54 -0300

    conversor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface CurrentPrice {
+  time: Time;
+  disclaimer: string;
+  chartName: string;
+  bpi: Bpi;
+}
+
+export interface Bpi {
+  USD: Rate;
+  GBP: Rate;
+  EUR: Rate;
+}
+
+export interface Rate {
+  code: string;
+  symbol: string;
+  rate: string;
+  description: string;
+  rate_float: number;
+}
+
+export interface Time {
+  updated: string;
+  updatedISO: Date;
+  updateduk: string;
+}
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +36,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public currentPrice: CurrentPrice;
+  public value = 0;
 
+  constructor(private http: HttpClient) {
+    this.loadPrices();
+  }
+  private async loadPrices() {
+    const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+    this.currentPrice = await this.http.get<CurrentPrice>(url).toPromise();
+  }
 }
