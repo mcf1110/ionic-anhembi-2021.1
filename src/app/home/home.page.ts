c633801d6a4fff14a0ecc7d43049eb4903b8381c
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-05-12 21:08:11 -0300

    add android

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Plugins, CameraResultType } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public image = ''
+
+  constructor() { }
+
+  public async getPhoto() {
+    const photo = await Plugins.Camera.getPhoto({
+      resultType: CameraResultType.DataUrl,
+    });
+    this.image = photo.dataUrl;
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { defineCustomElements } from "@ionic/pwa-elements/loader";
+
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +12,5 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+defineCustomElements(window);
