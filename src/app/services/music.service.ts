33c6982378e33f1b1bc35bf2e01e6c48d26189ac
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-05-26 19:51:14 -0300

    corrigindo http

--- src/app/services/music.service.ts
+++ src/app/services/music.service.ts
@@ -11,12 +11,12 @@ export class MusicService {
   constructor(private http: HttpClient) { }
 
   public searchArtists(query: string) {
-    const url = `http://musicbrainz.org/ws/2/artist/?query=${query}&fmt=json`
+    const url = `//musicbrainz.org/ws/2/artist/?query=${query}&fmt=json`
     return this.http.get<ArtistSearchResult>(url).toPromise();
   }
 
   public getReleases(id: string) {
-    const url = `http://musicbrainz.org/ws/2/artist/${id}?inc=release-groups&fmt=json`
+    const url = `//musicbrainz.org/ws/2/artist/${id}?inc=release-groups&fmt=json`
     return this.http.get<ArtistInfoResults>(url).toPromise();
   }
 }
